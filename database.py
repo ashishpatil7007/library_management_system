d0c5ae7567c295e112fbc484c137e395242882e4
Ashish Patil <ashishpatil7007@gmail.com>
2022-09-14 17:17:32 +0530

    comment updated

--- database.py
+++ database.py
@@ -1,28 +1,35 @@
 import sqlite3
 import pandas
-def insert_user(name,username,password,type):
-    #Connecting to sqlite
+
+
+def insert_user(name, username, password, type):
+    # Connecting to sqlite
     print("conecting to database")
     conn = sqlite3.connect('library_management_system_DB.db')
 
-    #Creating a cursor object using the cursor() method
+    # Creating a cursor object using the cursor() method
     cursor = conn.cursor()
 
     # Preparing SQL queries to INSERT a record into the database
     print("excuting sql query")
     cursor.execute('''INSERT INTO USER(
        NAME,USER_NAME,PASSSWORD,USER_TYPE) VALUES 
-       (\''''+name+'''\',\''''+username+'''\',\''''+password+'''\',\''''+type+'''\')''')
+       (\'''' + name + '''\',\'''' + username + '''\',\'''' + password + '''\',\'''' + type + '''\')''')
     conn.commit()
     print("excuting query is done")
-def validate_user(username,password):
+
+
+def validate_user(username, password):
     # Connecting to sqlite
     conn = sqlite3.connect('library_management_system_DB.db')
 
     # Creating a cursor object using the
     # cursor() method
     cursor = conn.cursor()
-    df= pandas.read_sql(f'''select * from USER where USER_NAME='{username}' and PASSSWORD='{password}' '''.format(username=username,password=password),conn)
+    df = pandas.read_sql(
+        f'''select * from USER where USER_NAME='{username}' and PASSSWORD='{password}' '''.format(username=username,
+                                                                                                  password=password),
+        conn)
     print(df)
 
     if df.empty:
@@ -30,31 +37,32 @@ def validate_user(username,password):
     else:
         return True
 
-def insert_books(book_name,author_name,book_price):
-    #Connecting to sqlite
+
+def insert_books(book_name, author_name, book_price):
+    # Connecting to sqlite
     print("conecting to database")
     conn = sqlite3.connect('library_management_system_DB.db')
 
-    #Creating a cursor object using the cursor() method
+    # Creating a cursor object using the cursor() method
     cursor = conn.cursor()
 
     # Preparing SQL queries to INSERT a record into the database
     print("excuting sql query")
     cursor.execute('''INSERT INTO BOOKS(
        BOOK_NAME,AUTHOR_NAME,PRICE) VALUES 
-       (\''''+book_name+'''\',\''''+author_name+'''\',\''''+book_price+'''\')''')
+       (\'''' + book_name + '''\',\'''' + author_name + '''\',\'''' + book_price + '''\')''')
     conn.commit()
     print("excuting query is done")
 
+
 def view_books():
-        # Connecting to sqlite
-        print("conecting to database")
-        conn = sqlite3.connect('library_management_system_DB.db')
+    # Connecting to sqlite
+    print("conecting to database")
+    conn = sqlite3.connect('library_management_system_DB.db')
 
-        # Creating a cursor object using the cursor() method
-        cursor = conn.cursor()
-        df = pandas.read_sql("select * from BOOKS", con=conn)
-        print(df)
-        conn.close()
-        return df.to_dict("records")
-
+    # Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+    df = pandas.read_sql("select * from BOOKS", con=conn)
+    print(df)
+    conn.close()
+    return df.to_dict("records")
--- main.py
+++ main.py
@@ -1,38 +1,55 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import os
+
 app = Flask(__name__)
 
+
+# this route is for login page
 @app.route('/')
 def login():
- return render_template("login.html")
+    # render login.html page
+    return render_template("login.html")
 
+
+# this route is for signup page
 @app.route('/signup')
 def signup():
- return render_template("signup.html")
+    # render signup.html page
+    return render_template("signup.html")
 
+
+# this route is for add_books page
 @app.route('/add_books')
 def add_books():
- return render_template("add_books.html")
+    # render add_books.html page
+    return render_template("add_books.html")
 
+
+# this route is for view_books page
 @app.route('/view_books')
 def view_books():
     from database import view_books
     books_dict = view_books()
     print(books_dict)
+    # render view_books.html page
     return render_template("view_books.html", books_dict=books_dict)
 
 
-@app.route('/API/save_user',methods=['post'])
+# this route is for save_user page
+@app.route('/API/save_user', methods=['post'])
 def save_user():
     name = request.form.get('name')  # access the data inside
     username = request.form.get('username')
     password = request.form.get('password')
     type = request.form.get('type')
-    print(name,username,password,type)
+    print(name, username, password, type)
     from database import insert_user
-    insert_user(name,username,password,type)
+    insert_user(name, username, password, type)
+    # render signup.html page
     return render_template("signup.html")
 
+
+# this route is for api/login page
 @app.route('/API/login', methods=["post"])
 def api_login():
     username = request.form.get('username')
@@ -42,25 +59,29 @@ def api_login():
     from database import validate_user
     is_valid_user = validate_user(username, password)
     if is_valid_user == True:
+        # render add_books.html page
         return render_template("add_books.html")
-
+    # render login.html page
     return render_template("login.html")
 
-@app.route('/API/save_books',methods=['post'])
+
+# this route is for api/save_books page
+@app.route('/API/save_books', methods=['post'])
 def save_books():
     book_name = request.form.get('book_name')  # access the data inside
     author_name = request.form.get('author_name')
     book_price = request.form.get('book_price')
-   
-    print(book_name,author_name,book_price)
+
+    print(book_name, author_name, book_price)
     from database import insert_books
-    insert_books(book_name,author_name,book_price)
+    insert_books(book_name, author_name, book_price)
+    # render add_books.html page
     return render_template("add_books.html")
 
 
 if __name__ == '__main__':
-   if 'PORT' in os.environ:
-      port = os.environ['PORT']
-   else:
-      port= 5000
-   app.run(debug=True,host="0.0.0.0",port=port)
+    if 'PORT' in os.environ:
+        port = os.environ['PORT']
+    else:
+        port = 5000
+    app.run(debug=True, host="0.0.0.0", port=port)
